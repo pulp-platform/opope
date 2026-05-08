73f2dad099f22028a535d7fa37cc90621b7ffbde
Danilo Cammarata <dcammarata@iis.ee.ethz.ch>
2026-05-08 15:59:41 +0200

    [LINT] Fixing RTL license

--- target/sim/vsim/save_waves.tcl
+++ target/sim/vsim/save_waves.tcl
@@ -1,2 +1,10 @@
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Danilo Cammarata <dcammarata@iis.ee.ethz.ch>
+#
+# Script to automatically save the wabeforms in O-POPE
+
 write format wave -window .main_pane.wave.interior.cs.body.pw.wf wave.do
 quit -f
