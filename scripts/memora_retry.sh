ec87836b0d2977f4242a9f54a2348382b0909500
Danilo Cammarata <dcammarata@iis.ee.ethz.ch>
2026-06-02 11:39:08 +0200

    [CI] Fixing CI - python script

--- scripts/memora_retry.sh
+++ scripts/memora_retry.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
-
-# Copyright 2021 ETH Zurich and University of Bologna.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE_SW for details.
 # SPDX-License-Identifier: Apache-2.0
+#
+# Danilo Cammarata <dcammarata@iis.ee.ethz.ch>
+#
 
 i=1
 max_attempts=10
